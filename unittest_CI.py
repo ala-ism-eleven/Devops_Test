68e2b817b2913fbe51662961f07c4db346bf1ad7
ala-ism <ismail.alaoui@eleven-strategy.com>
2020-10-15 14:37:12 +0200

    ok

---
+++ unittest_CI.py
@@ -0,0 +1,18 @@
+"""
+Unit tests for the calculator library
+"""
+
+import test_Circle_CI
+import unittest
+
+
+class TestCalculator(unittest.TestCase):
+
+    def test_addition(self):
+        self.assertEqual(4, test_Circle_CI.add(2, 2))
+
+    def test_subtraction(self):
+        self.assertEqual(2, test_Circle_CI.subtract(4, 2))
+
+if __name__=="__main__":
+    unittest.main()
