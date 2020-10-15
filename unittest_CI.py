62203c3070f81f7e52c793c41a0ebbe4052b2031
ala-ism-eleven <56955471+ala-ism-eleven@users.noreply.github.com>
2020-10-15 14:41:01 +0200

    Update unittest_CI.py

--- unittest_CI.py
+++ unittest_CI.py
@@ -5,14 +5,19 @@ Unit tests for the calculator library
 import test_Circle_CI
 import unittest
 
-
 class TestCalculator(unittest.TestCase):
-
+#class desc
+    
     def test_addition(self):
+    #func desc    
+        
         self.assertEqual(4, test_Circle_CI.add(2, 2))
 
     def test_subtraction(self):
+    #func desc    
+        
         self.assertEqual(2, test_Circle_CI.subtract(4, 2))
 
-if __name__=="__main__":
+if __name__ == "__main__":
     unittest.main()
+    
